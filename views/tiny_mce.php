6bc59bddf71fa359c67f94c50e9fe2f47cc1f7fb
timenz <timenz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-01-05 02:43:29 +0000

    setting ok, add shortcode ok
    git-svn-id: https://plugins.svn.wordpress.org/mailtarget-form/trunk@1797352 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/tiny_mce.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript"
+	        src="<?php echo site_url() . '/wp-includes/js/tinymce/tiny_mce_popup.js'; ?>"></script>
+</head>
+
+<body>
+
+<h2>Add form</h2>
+
+<form id="mailtarget_tinymce_form" action="" method="post">
+
+	<p><?php _e('Select form from list below, and hit "Add Shortcode" to add the shortcode to your post!', 'mailtarget'); ?></p>
+
+	<p>
+		<label for="mailtarget_form_id">Form</label><br/>
+		<select class="widefat" id="mailtarget_form_id" name="mailtarget_form_id">
+			<?php foreach ($forms as $form): ?>
+				<option value="<?php echo $form->id; ?>"><?php echo $form->name; ?></option>
+			<?php endforeach; ?>
+		</select>
+	</p>
+
+	<input type="button" name="<?php _e('Add Shortcode', 'mailtarget'); ?>" value="Add Shortcode">
+
+</form>
+<script type="text/javascript">
+    jQuery(document).ready(function () {
+        jQuery('form#mailtarget_tinymce_form input:button').click(function () {
+            var form_id = jQuery('form#mailtarget_tinymce_form #mailtarget_form_id').val();
+            var shortcode = '[mailtarget_form form_id=' + form_id + ']';
+            tinyMCEPopup.execCommand("mceInsertContent", false, shortcode);
+            tinyMCEPopup.close();
+            return false;
+        });
+    });
+</script>
+</body>
+</html>
