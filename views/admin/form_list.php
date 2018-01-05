847651c05ebe95a5b9556df063eb74b1a7e93e76
timenz <timenz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-01-05 02:48:23 +0000

    add paging to list form remote and add error message for form with captcha
    git-svn-id: https://plugins.svn.wordpress.org/mailtarget-form/trunk@1797366 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/form_list.php
+++ views/admin/form_list.php
@@ -3,6 +3,8 @@ $targetPage = 'mailtarget-form-plugin--admin-menu-widget-add';
 if (isset($_GET['for'])) {
     if ($_GET['for'] == 'popup') $targetPage = 'mailtarget-form-plugin--admin-menu-popup-main';
 }
+$pg = 1;
+if (isset($_GET['pg'])) $pg = $_GET['pg'];
 ?>
 <div class="wrap mtg-form-plugin">
     <?php include MAILTARGET_PLUGIN_DIR . '/views/admin/style.php' ?>
@@ -12,7 +14,7 @@ if (isset($_GET['for'])) {
     <h1 class="wp-heading-inline">Select Form - Mailtarget Form</h1>
     <p>Select MailTarget Form to setup</p>
 
-    <?php if (count($forms) < 1) {?>
+    <?php if (count($forms['data']) < 1) {?>
             <div class="update-nag">List empty, start by
                 <a href="admin.php?page=mailtarget-form-plugin--admin-menu-widget-add">creating one</a></div><?php
     } else {
@@ -20,27 +22,49 @@ if (isset($_GET['for'])) {
         <table class="wp-list-table widefat fixed striped pages">
         <thead>
         <tr>
-            <th>No</th>
-            <th>Name</th>
+            <th width="5%">No</th>
             <th>Created At</th>
+            <th>Name</th>
+            <th>Status</th>
             <th>Action</th>
         </tr>
         </thead>
         <?php
-        $no = 1;
+        $no = (10 * ($pg - 1));
         foreach ($forms['data'] as $item) {
+            $no++;
             ?>
             <tr>
                 <td><?php echo $no ?></td>
-                <td><?php echo $item['name'] ?></td>
                 <td><?php echo date('Y-m-d H:i', $item['createdAt']/1000) ?></td>
-                <td><a href="admin.php?page=<?php echo $targetPage ?>&form_id=<?php echo $item['formId'] ?>">Select</a></td>
+                <td><?php echo $item['name'] ?></td>
+                <td><?php
+                    $status = ($item['published']) ? 'published' : 'not published';
+                    if ($item['setting']['captcha']) $status .= ', captcha enabled';
+                    echo $status;
+                    ?></td>
+                <td>
+                    <?php if ($item['published']) {
+                        ?><a href="admin.php?page=<?php echo $targetPage ?>&form_id=<?php echo $item['formId'] ?>">Select</a><?php
+                    } else {
+                        ?><?php
+                    } ?>
+
+                </td>
             </tr>
             <?php
-            $no++;
         }
 
-        ?></table><?php
+        ?></table>
+        <div class="nav" style="margin-top: 15px;">
+            <?php
+            if ($pg > 1) {
+                ?><a class="page-title-action" href="admin.php?page=mailtarget-form-plugin--admin-menu-widget-form&pg=<?php echo ($pg - 1) ?>">previous</a> <?php
+            }
+            if (count($forms['data']) > 9) {
+                ?><a class="page-title-action" style="float: right" href="admin.php?page=mailtarget-form-plugin--admin-menu-widget-form&pg=<?php echo ($pg + 1) ?>">next</a><?php
+            }
+        ?></div><?php
     }
     ?>
 </div>
