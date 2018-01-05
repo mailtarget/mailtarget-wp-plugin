0d6cdb41f42ce39a488acb0d758a53bb4ed400bc
timenz <timenz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-01-05 02:44:15 +0000

    updating components
    git-svn-id: https://plugins.svn.wordpress.org/mailtarget-form/trunk@1797354 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/render/input_textarea.php
+++ views/render/input_textarea.php
@@ -3,10 +3,10 @@ $setting = $row['setting'];
 //print_r($setting);
 ?>
 <div class="mt-c-form__wrap">
-    <div div class="mt-c-form__input">
+    <div div class="mt-c-form__textarea">
 		<?php if ($setting['showTitle']) { ?>
-            <label class="mt-o-label" v-if="setting.showTitle"><?php echo $setting['title'].$setting['fieldType'] ?></label>
+            <label class="mt-o-label" v-if="setting.showTitle"><?php echo $setting['title'] ?></label>
 		<?php } ?>
-        <input type="<?php echo $setting['fieldType']; ?>" class="mt-o-input" :placeholder="<?php echo $setting['description']; ?>">
+        <textarea class="mt-o-textarea" :placeholder="<?php echo $setting['description']; ?>"></textarea>
     </div>
 </div>
