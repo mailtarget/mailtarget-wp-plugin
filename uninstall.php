2f88f431a462f32219cf03f484c425939dca3916
Bagas Ghufron Alfaiz <bagasghufronalfaiz@gmail.com>
2023-03-29 13:57:25 +0700

    delete data on deleting plugin

--- uninstall.php
+++ uninstall.php
@@ -9,8 +9,20 @@
  * @link      {{author_url}}
  */
 
-/* If uninstall is not called from WordPress exit. */
+// if uninstall.php is not called by WordPress, die
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit();
+	die;
 }
-/* Place uninstall code below here. */
+
+delete_option( 'mtg_company_id' );
+delete_option( 'mtg_api_token' );
+delete_option( 'mtg_popup_enable' );
+delete_option( 'mtg_popup_form_id' );
+delete_option( 'mtg_popup_form_name' );
+delete_option( 'mtg_popup_delay' );
+delete_option( 'mtg_popup_title' );
+delete_option( 'mtg_popup_description' );
+delete_option( 'mtg_popup_redirect' );
+
+global $wpdb;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->base_prefix}mailtarget_forms" );
